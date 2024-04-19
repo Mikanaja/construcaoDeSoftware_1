1d5b0545c55e44f7b0f8f6c208351da2f14c83aa
Mikhael Machado Fernandes Maia <65919190+Mikanaja@users.noreply.github.com>
2024-04-19 14:45:54 -0300

    Create Endereco.java

---
+++ Endereco.java
@@ -0,0 +1,14 @@
+public class Endereco {
+    private int numero;
+    private String complemento;
+    private int CEP;
+    private TipoLogradouro tipoLogradouro;
+    private TipoEndereco tipoEndereco;
+    private Bairro bairro;
+    private Cidade cidade;
+    private Estado estado;
+    private Pais pais;
+    private PessoaFisica pessoaFisica; // Associação com PessoaFisica
+
+    // Métodos getters e setters
+}
