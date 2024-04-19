83529ceb182f26a3660ecae7310f14b1566efb34
Mikhael Machado Fernandes Maia <65919190+Mikanaja@users.noreply.github.com>
2024-04-19 15:07:33 -0300

    Create Estado.java

---
+++ Estado.java
@@ -0,0 +1,8 @@
+public class Estado {
+    public String sigla;
+    private String nome;
+    private List<Cidade> cidades; // Associação com Cidade
+    private Pais pais; // Associação com Pais
+
+    // Métodos getters e setters
+}
