aa5b186ca3db4b95aa27a0890f4189ff117a8da1
Mikhael Machado Fernandes Maia <65919190+Mikanaja@users.noreply.github.com>
2024-04-19 15:04:38 -0300

    Create Cidade.java

---
+++ Cidade.java
@@ -0,0 +1,9 @@
+import java.util.List;
+
+public class Cidade {
+    private String nome;
+    private List<Bairro> bairros; // Associação com Bairro
+    private Estado estado; // Associação com Estado
+
+    // Métodos getters e setters
+}
