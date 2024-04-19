af0a3e01c5bb29a5ba6ee09fa0a20e8a9d5a2730
Mikhael Machado Fernandes Maia <65919190+Mikanaja@users.noreply.github.com>
2024-04-19 14:44:59 -0300

    Create PessoaFisica.java

---
+++ PessoaFisica.java
@@ -0,0 +1,12 @@
+// Arquivo: PessoaFisica.java
+import java.util.Date;
+import java.util.List;
+
+public class PessoaFisica {
+    private String nome;
+    private String sexo;
+    private Date dataNascimento;
+    private List<Endereco> enderecos;
+
+    // Métodos getters e setters
+}
